a19c7cd0db607c8afc9dfad5993ba9adbf9cd0bc
Tobias Pingel <tobias.pingel@googlemail.com>
2023-02-26 19:36:01 +0100

    added react dom

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const index = () => {
+const Home = () => {
   return (
-    <div>index</div>
+    <>
+      Herobanner
+      <div className="products-heading">
+        <h2>Best selling products.</h2>
+        <p>Speakers of many variatons.</p>
+      </div>
+      <div className="products-container">
+        {['product1', 'product2'].map((product)=>product)}
+      </div>
+
+      Footer
+
+    </>
   )
 }
 
-export default index
+export default Home
