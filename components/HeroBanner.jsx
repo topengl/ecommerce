d5d5cace42426cb31709ac8e257f316aca975ffb
Tobias Pingel <tobias.pingel@googlemail.com>
2023-03-05 10:09:03 +0100

    adapt style

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -9,19 +9,20 @@ const HeroBanner = ({heroBanner}) => {
   return (
     <div className="hero-banner-container">
         <div>
-            <p className="beats-solo">{heroBanner.smallText}</p>
+            <img src={urlFor(heroBanner.image)} alt="Headphones" className="hero-banner-image" />
+            <p className="small-text">{heroBanner.smallText}</p>
             <h3>{heroBanner.midText}</h3>
             <h1>{heroBanner.largeText1}</h1>
-            <img src={urlFor(heroBanner.image)} alt="Headphones" className="hero-banner-image" />
-        <div>
-            <Link href={`/product/ID${heroBanner.product}`}>
-                <button type="button">{heroBanner.buttonText}</button>
-            </Link>
-            <div className="desc">
-                <h5>Description</h5>
-                <p>{heroBanner.desc}</p>
+            
+            <div>
+                <Link href={`/product/ID${heroBanner.product}`}>
+                    <button type="button">{heroBanner.buttonText}</button>
+                </Link>
+                <div className="desc">
+                    <h5>Description</h5>
+                    <p>{heroBanner.desc}</p>
+                </div>
             </div>
-        </div>
         </div>
     </div>
   )
