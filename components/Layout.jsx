b72be97d1b387633974de70450a6c59c2fcfb718
Tobias Pingel <tobias.pingel@googlemail.com>
2023-03-06 07:52:54 +0100

    start layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+/* Head is the same thing, as head is in html
+which is located above the body and gives us metadata for the website */
+import Head from  'next/head';
 
 const Layout = () => {
   return (
