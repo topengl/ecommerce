44b24c09f57efba55b6adac36fe435f54920368b
topin <topin@kkwindsolutions.com>
2023-03-06 09:39:56 +0100

    How to we make that Component appear inside our Layout in React? -> Through a prop children

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,27 @@
+
 import React from 'react';
 /* Head is the same thing, as head is in html
 which is located above the body and gives us metadata for the website */
 import Head from  'next/head';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
-const Layout = () => {
+const Layout = ({children}) => {
   return (
-    <div>Layout</div>
+    <div className="layout">
+      <Head>
+        <title>JS Binkaboooo Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
+/*The _app file is called during each page initialization. 
+Within the _app file you can fully control the page initialization process 
+by implementing custom logic 
+to make your pages consistent across your web property. */
+import { Layout } from '../components';
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) 
+{
+  return(
+    <Layout>
+      {/* How to we make that Component appear inside our Layout in React? -> Through a prop children */}
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
+
+  export default MyApp
