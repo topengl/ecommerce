d74e5b20f009ed5558a0c0da12b650a33abc280a
topin <topin@kkwindsolutions.com>
2023-03-18 19:27:07 +0100

    Buy now button

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -13,7 +13,11 @@ import { useStateContext } from '../../context/StateContext';
 const ProductDetails = ({product, products}) => {
     const{image, name, details, price} = product;
     const [index, setIndex] = useState(0); // at start we want to look at the image under the index of 0
-    const { decQty, incQty, qty, onAdd } = useStateContext(); // we can use useStateContext like a hook and here we destructure the values
+    const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext(); // we can use useStateContext like a hook and here we destructure the values
+    const handleBuyNow = () =>{
+        onAdd(product, qty);
+        setShowCart(true);
+    }
   return (
     <div>
         <div className="product-detail-container">
@@ -67,7 +71,7 @@ const ProductDetails = ({product, products}) => {
                 </div>
                 <div className="buttons">
                     <button type="button" className="add-to-cart" onClick={() => onAdd(product,qty)}>Add to Cart</button>
-                    <button type="button" className="buy-now" >Buy Now</button> {/* topin: onClick="" */}
+                    <button type="button" className="buy-now" onClick={handleBuyNow}>Buy Now</button> {/* topin: onClick="" */}
                 </div>
             </div>
         </div>
