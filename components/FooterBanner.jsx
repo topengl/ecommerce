e76c465e232b1d8a1cf2d6cf95caf1c3f6a54e8a
topin <topin@kkwindsolutions.com>
2023-03-20 21:07:07 +0100

    style

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -8,24 +8,26 @@ import {urlFor} from '../lib/client'
 const FooterBanner = ({footerBanner : {discount, largeText1, largeText2, saleTime, buttonText, image, product, desc, smallText, midText} }) => {
   return (
     <div className="footer-banner-container">
-      <div className="banner-desc">
-        <div className="left">
-          {/* the p tag is going to render the discount coming from sanity banner elements*/}
-          <p>{discount}</p>
-          <h3>{largeText1}</h3>
-          <h2>{largeText2}</h2>
-          <p>{ saleTime}</p>
-        </div>
-        <div className="right">
-          <p>{smallText}</p>
-          <h3>{midText}</h3>
-          <p>{desc}</p>
+      <div className="left">
+        {/* the p tag is going to render the discount coming from sanity banner elements*/}
+        <p>{saleTime}</p>
+        <p>{discount}</p>
+        <h1>{largeText1}</h1>
+        <h2>{largeText2}</h2>
+        <div className="buttonBox"> 
           {/* dynamic link for the product */}
           <Link href={`/product/${product}`}>
             <button type="button">{buttonText}</button>
           </Link>
         </div>
-        <img src={urlFor(image)} className="footer-banner-image"/>
+      </div>
+      <div className="right">
+        <p>{smallText}</p>
+        <h3>{midText}</h3>
+        <p>{desc}</p>
+        <div className="imageBox">
+          <img src={urlFor(image)} className="footer-banner-image"/>
+        </div>
       </div>
     </div>
   )
--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -8,23 +8,24 @@ import {urlFor} from '../lib/client';
 const HeroBanner = ({heroBanner}) => {
   return (
     <div className="hero-banner-container">
-        <div>
-            <img src={urlFor(heroBanner.image)} alt="Headphones" className="hero-banner-image" />
+      <div>
+        <img src={urlFor(heroBanner.image)} alt="Headphones" className="hero-banner-image" />
+          <div className="hero-banner-text-left">
             <p className="small-text">{heroBanner.smallText}</p>
             <h3>{heroBanner.midText}</h3>
             <h1>{heroBanner.largeText1}</h1>
-            
-            <div>
-                <Link href={`/product/${heroBanner.product}`}>  {/* topin: the link has to be the same as for the product. pros and cons for using aditional banner content, then fetch data from the Product content? */}
-                {/*<Link href='/product/flexible_100w_photovoltaic_module'>*/}
-                    <button type="button">{heroBanner.buttonText}</button>
-                </Link>
-                <div className="desc">
-                    <h5>Description</h5>
-                    <p>{heroBanner.desc}</p>
-                </div>
-            </div>
+          </div>    
+        <div>
+          <Link href={`/product/${heroBanner.product}`}>  {/* topin: the link has to be the same as for the product. pros and cons for using aditional banner content, then fetch data from the Product content? */}
+          {/*<Link href='/product/flexible_100w_photovoltaic_module'>*/}
+              <button type="button">{heroBanner.buttonText}</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{heroBanner.desc}</p>
+          </div>
         </div>
+      </div>
     </div>
   )
 }
