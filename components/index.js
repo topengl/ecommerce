ea75f251d60d5aba47376164a467d61142dcf018
topin <tobias.pingel@googlemail.com>
2023-05-08 11:29:00 +0200

    delete hero banner

--- components/index.js
+++ components/index.js
@@ -2,7 +2,6 @@
 // export keyword from one the one of the same files would be ignored, if we use a different name
 export {default as Footer} from './Footer';
 export {default as FooterBanner} from './FooterBanner';
-export {default as HeroBanner} from './HeroBanner';
 export {default as Layout} from './Layout';
 export {default as Navbar} from './Navbar';
 export {default as Product} from './Product';
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
 import React from 'react'
 
 import {client} from '../lib/client';
-import {FooterBanner, HeroBanner, Product} from '../components';
+import {Product, FooterBanner} from '../components';
 
 const Home = ({products, bannerData}) => {
   return (
     <>
-      <HeroBanner heroBanner = {bannerData.length && bannerData[0] } /> {/* if bannerData Array exists, we gonna pass the first element of bannerData */}
-      {/* console.log(bannerData) */}
-
       <div className="products-heading">
         <h2>Products</h2>
         <p>Flexible Photowoltaics for the balcony.</p>
@@ -17,9 +14,6 @@ const Home = ({products, bannerData}) => {
       <div className="products-container">
         {products?.map((product)=><Product key={product._id} product ={product}/>)}
       </div>
-
-      {/* FooterBanner is a JSX element. The JSX element is here given the property footerBanner. The property gets returned bannerData[0], if bannerData exists. J*/}
-      <FooterBanner footerBanner={bannerData && bannerData[0]} />
 
     </>
   )
