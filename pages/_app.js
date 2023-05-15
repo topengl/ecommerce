3b7e1067e3cb8569bed1de4bb49abca95bd5421e
topin <tobias.pingel@googlemail.com>
2023-05-15 13:14:54 +0200

    start adding cookies

--- pages/_app.js
+++ pages/_app.js
@@ -2,36 +2,40 @@
 Within the _app file you can fully control the page initialization process 
 by implementing custom logic 
 to make your pages consistent across your web property. */
-import { Layout } from '../components';
 import '@/styles/globals.css';
+import { Layout } from '../components';
 import {StateContext } from '../context/StateContext';
 import { Toaster } from 'react-hot-toast'; // is a module that helps providing pop ups on a website
-import CookieConsent from 'react-cookie-consent';
+import { CookieConsent } from 'react-cookie-consent';
+import { CookiesProvider } from 'react-cookie';
 
 function MyApp({ Component, pageProps }) 
 {
   return(
     //to have the state values from context/StateContext.js available, we wrap Layout in StateContext
     <StateContext>
-      <Layout>
-        <Toaster />
-        {/* How to we make that Component appear inside our Layout in React? -> Through a prop children */}
-        <Component {...pageProps} />
-        <CookieConsent
-          location="bottom"
-          buttonText="ok"
-          cookieName="myAwesomeCookieName2"
-          style={ { background: "var(--background-all-2)",
-                    color: "var(--color-all)" }}
-          buttonStyle={{  color: "var(--color-all)", 
-                          fontSize: "13px",
-                          background: "var(--bright-red)"}}
-          expires={150}
-        >
-          This website uses cookies to enhance the user experience.{" "}
-          <span style={{ fontSize: "10px" }}> ... </span>
-      </CookieConsent>
-      </Layout>
+      <CookiesProvider>
+        
+        <Layout>
+          <Toaster />
+          {/* How to we make that Component appear inside our Layout in React? -> Through a prop children */}
+          <Component {...pageProps} />
+          <CookieConsent
+            location="bottom"
+            buttonText="ok"
+            cookieName="myAwesomeCookieName2"
+            style={ { background: "var(--background-all-2)",
+                      color: "var(--color-all)" }}
+            buttonStyle={{  color: "var(--color-all)", 
+                            fontSize: "13px",
+                            background: "var(--bright-red)"}}
+            expires={150}
+          >
+            This website uses cookies to enhance the user experience.{" "}
+            <span style={{ fontSize: "10px" }}> ... </span>
+        </CookieConsent>
+        </Layout>
+      </CookiesProvider>
     </StateContext>
   )
 }
